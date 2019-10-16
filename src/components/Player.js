1cf6b669bc3275f1da44312eb8765b450af1efb0
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2019-10-16 19:55:57 -0400

    improved async operation

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import Login from './Login';
@@ -11,38 +11,39 @@ import './Player.scss';
 const Player = ({ token, dispatch }) => {
   let player;
 
-  const [loc, setLoc] = useState('q=New York, NY, US');
   const [desc, setDesc] = useState();
 
-  const coords = (pos) => {
-    setLoc(`lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`);
-  };
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(coords);
-  }
-  const getWeather = () => {
-    const url = `https://community-open-weather-map.p.rapidapi.com/weather?${loc}&units=imperial`;
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': '15fe13f990msha0b23062c32707cp148f84jsn17e58c929a3f',
-      },
-    }).then((response) => response.json())
-      .catch((err) => console.log(err))
-      .then((json) => {
-        dispatch(updateWeather(json));
-        setDesc(`Music based on ${json.name} weather...`);
-        // createPlayer();
+  // Fetch weather data based on location
+  useEffect(() => {
+    let loc = 'q=New York, NY, US';
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        loc = `lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`;
       });
-  };
+    }
+
+    (async () => {
+      const url = `https://community-open-weather-map.p.rapidapi.com/weather?${loc}&units=imperial`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          'x-rapidapi-key': '15fe13f990msha0b23062c32707cp148f84jsn17e58c929a3f',
+        },
+      });
+      const json = await response.json();
+      dispatch(updateWeather(json));
+      setDesc(`Music based on ${json.name} weather...`);
+    })();
+  }, [dispatch]);
 
   if (token) {
     player = (
       <div className="player__container">
         <h1>Player</h1>
         {/* <input onChange={(e) => setLoc(e.target.value)} value={loc} /> */}
-        <button type="submit" onClick={getWeather}>Submit</button>
+        {/* <button type="submit" onClick={getWeather}>Submit</button> */}
         <h1>{ desc }</h1>
       </div>
     );
