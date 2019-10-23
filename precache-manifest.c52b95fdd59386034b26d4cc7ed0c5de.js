ea6a5ad01af06e567bf4ebbb72b3b16b22f79fd7
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2019-10-23 15:39:42 -0400

    Updates

---
+++ precache-manifest.c52b95fdd59386034b26d4cc7ed0c5de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15d9f676a6ff63819a6e74a4466216b",
+    "url": "/spotty-sky/index.html"
+  },
+  {
+    "revision": "f30d570a3e7254823e7e",
+    "url": "/spotty-sky/static/css/main.9054eb22.chunk.css"
+  },
+  {
+    "revision": "2ff953b8ac70adcf7ac9",
+    "url": "/spotty-sky/static/js/2.e4e9fd60.chunk.js"
+  },
+  {
+    "revision": "f30d570a3e7254823e7e",
+    "url": "/spotty-sky/static/js/main.f39c17c6.chunk.js"
+  },
+  {
+    "revision": "794cd4a43261705a0131",
+    "url": "/spotty-sky/static/js/runtime-main.aab477a3.js"
+  }
+]);
